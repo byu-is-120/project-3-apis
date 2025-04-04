801eef89cd804fac9911aa3dd2999396a4843226
corban <corbanpro@gmail.com>
2025-04-04 13:54:47 -0600

    update api upload scripts

--- api-handler/upload.sh
+++ api-handler/upload.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [[ -z "$API_FOLDER" ]]; then
+	echo "Error: API_FOLDER is not set."
+	exit 1
+fi
+
 echo "Uploading $API_FOLDER"
 
 aws lambda update-function-code \
 	--function-name $API_FOLDER \
-	--zip-file fileb://./dist-$API_FOLDER.zip \
-	--no-cli-pager
+	--zip-file fileb://./dist/$API_FOLDER.zip \
+	--no-cli-pager # don't require the user to press enter to continue
--- api-handler/zip.sh
+++ api-handler/zip.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+if [[ -z "$API_FOLDER" ]]; then
+	echo "Error: API_FOLDER is not set."
+	exit 1
+fi
+
 echo "Zipping $API_FOLDER"
 
-mv ./index.js ./tmp-index.js
+# move the index.js file to a temporary file. This makes sure the source code isn't modified or lost
+cp ./index.js ./tmp-index.js
 
-sed "s/Key: \".*\",/Key: \"data\/${API_FOLDER}\/data.json\",/g" ./tmp-index.js >./index.js
+# Update the index.js file to point to the correct folder in S3
+sed -i "s/Key: \".*\",/Key: \"data\/${API_FOLDER}\/data.json\",/g" ./index.js
 
-zip -rq dist-${API_FOLDER}.zip . -x "*.zip"
+# Create a zip file excluding the other zip files
+zip -rq ./dist/${API_FOLDER}.zip . -x "*.zip" -x "dist/*"
 
+# move the index.js file back to the original path
 mv ./tmp-index.js ./index.js
