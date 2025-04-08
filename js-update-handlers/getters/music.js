bbf01dd9e5a173ddbb51c9c651f798f02dd2f59d
corban <corbanpro@gmail.com>
2025-04-07 18:20:34 -0600

    music api caching

--- js-update-handlers/getters/music.js
+++ js-update-handlers/getters/music.js
@@ -4,6 +4,8 @@ import base64 from "base-64";
 import { URLSearchParams } from "url";
 import { retrieveApiKeys } from "../utils/aws-secrets.js";
 import https from "https";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+const s3 = new S3Client({ region: "us-west-2" });
 
 const GENRE_LIMIT = 10;
 const ARTIST_PER_GENRE_LIMIT = 20;
@@ -12,7 +14,7 @@ const ALBUM_PER_ARTIST_LIMIT = 5;
 const agent = new https.Agent({
   keepAlive: true,
   timeout: 10000,
-  maxSockets: 10,
+  maxSockets: 5,
   maxFreeSockets: 5,
 });
 
@@ -27,6 +29,7 @@ class SpotifyDataCollector {
     this.clientSecret = clientSecret;
     this.token = null;
     this.baseUrl = "https://api.spotify.com/v1";
+    this.existingData = {};
   }
 
   async getToken() {
@@ -45,6 +48,31 @@ class SpotifyDataCollector {
     });
   }
 
+  async getExistingData() {
+    // Helper function to convert a ReadableStream to a string
+    async function streamToString(stream) {
+      const chunks = [];
+      for await (const chunk of stream) {
+        chunks.push(chunk);
+      }
+      return Buffer.concat(chunks).toString("utf-8");
+    }
+
+    const getObjectCommand = new GetObjectCommand({
+      Bucket: "is120-w25-apis",
+      Key: "data/music-api/data.json",
+    });
+
+    try {
+      const s3Res = await s3.send(getObjectCommand);
+      const existingDataJson = await streamToString(s3Res.Body);
+      this.existingData = JSON.parse(existingDataJson).data;
+    } catch (error) {
+      console.error("Error fetching existing data from S3:", error);
+      this.existingData = {};
+    }
+  }
+
   getHeaders() {
     return { Authorization: `Bearer ${this.token}` };
   }
@@ -53,7 +81,6 @@ class SpotifyDataCollector {
     const popularGenres = [
       "pop",
       "rock",
-      "hip hop",
       "dance",
       "electronic",
       "latin",
@@ -75,6 +102,31 @@ class SpotifyDataCollector {
     return popularGenres.slice(0, limit);
   }
 
+  async makeRequest(url, callback) {
+    return await axiosClient
+      .get(url, { headers: this.getHeaders() })
+      .then((response) => {
+        console.log(`Fetched data from URL: ${url}`);
+        return callback(response);
+      })
+      .catch((error) => {
+        if (error.code === "ETIMEDOUT") {
+          console.warn(`Timeout fetching data from URL: ${url}`);
+          return this.delay(1000).then(() => this.makeRequest(url, callback));
+        }
+        if (error?.response?.status === 429) {
+          const retryAfter =
+            (error.response.headers?.["retry-after"] || 10) + 1;
+          console.warn(`Rate limit exceeded, retrying in ${retryAfter}s...`);
+          return this.delay(retryAfter * 1000).then(() =>
+            this.makeRequest(url, callback),
+          );
+        } else {
+          throw error;
+        }
+      });
+  }
+
   async searchArtistsByGenre(genre, limit = ARTIST_PER_GENRE_LIMIT) {
     const endpoint = `${this.baseUrl}/search`;
     const queryParams = new URLSearchParams({
@@ -84,35 +136,13 @@ class SpotifyDataCollector {
     });
     const url = `${endpoint}?${queryParams.toString()}`;
 
-    const items = await axios
-      .get(url, { headers: this.getHeaders() })
-      .then((response) => {
-        console.log(`Fetched artists for genre: ${genre}`);
-        return response.data.artists.items;
-      })
-      .catch((error) => {
-        if (error.code === "ETIMEDOUT") {
-          console.warn(`Timeout fetching album details: ${albumId}`);
-          return this.delay(1000).then(() =>
-            this.searchArtistsByGenre(albumId),
-          );
-        }
-        if (error?.response?.status === 429) {
-          const retryAfter =
-            (error.response.headers?.["retry-after"] || 10) + 1;
-          console.warn(`Rate limit exceeded, retrying in ${retryAfter}s...`);
-          return this.delay(retryAfter * 1000).then(() =>
-            this.searchArtistsByGenre(genre, limit),
-          );
-        } else {
-          throw error;
-        }
-      });
-
-    return items;
+    return await this.makeRequest(
+      url,
+      (response) => response.data.artists.items,
+    );
   }
 
-  async getArtistAlbums(artistId, limit = ALBUM_PER_ARTIST_LIMIT) {
+  async getArtistAlbums(artistId, genreId, limit = ALBUM_PER_ARTIST_LIMIT) {
     const endpoint = `${this.baseUrl}/artists/${artistId}/albums`;
     const params = new URLSearchParams({
       include_groups: "album",
@@ -121,83 +151,58 @@ class SpotifyDataCollector {
     });
     const url = `${endpoint}?${params.toString()}`;
 
-    return await axios
-      .get(url, { headers: this.getHeaders() })
-      .then(async (response) => {
-        console.log(`Fetched albums for artist: ${artistId}`);
-        const albums = response.data.items;
-        const albumDetails = [];
-        for (const album of albums.slice(0, limit)) {
-          const details = await this.getAlbumDetails(album.id);
-          if (details) albumDetails.push(details);
-        }
-        albumDetails.sort((a, b) => b.popularity - a.popularity);
-        return albumDetails.slice(0, limit);
-      })
-      .catch((error) => {
-        if (error.code === "ETIMEDOUT") {
-          console.warn(`Timeout fetching album details: ${albumId}`);
-          return this.delay(1000).then(() => this.getArtistAlbums(albumId));
-        }
-        if (error?.response?.status === 429) {
-          const retryAfter =
-            (error.response.headers?.["retry-after"] || 10) + 1;
-          console.warn(`Rate limit exceeded, retrying in ${retryAfter}s...`);
-          return this.delay(retryAfter * 1000).then(() =>
-            this.getArtistAlbums(artistId, limit),
-          );
-        } else {
-          throw error;
-        }
-      });
+    return await this.makeRequest(url, async (response) => {
+      const albums = response.data.items;
+      const albumDetails = [];
+      for (const album of albums.slice(0, limit)) {
+        const details = await this.getAlbumDetails(album.id, genreId, artistId);
+        if (details) albumDetails.push(details);
+      }
+      albumDetails.sort((a, b) => b.popularity - a.popularity);
+      return albumDetails.slice(0, limit);
+    });
   }
 
-  async getAlbumDetails(albumId) {
-    const endpoint = `${this.baseUrl}/albums/${albumId}`;
+  async getAlbumDetails(albumId, genreId, artistId) {
+    const url = `${this.baseUrl}/albums/${albumId}`;
 
-    return await axios
-      .get(endpoint, {
-        headers: this.getHeaders(),
-      })
-      .then((response) => {
-        console.log(`Fetched details for album: ${albumId}`);
-        const albumData = response.data;
-        return {
-          name: albumData.name,
-          release_date: albumData.release_date,
-          total_tracks: albumData.total_tracks,
-          popularity: albumData.popularity || 0,
-          album_type: albumData.album_type,
-          cover_image: albumData.images[0]?.url || "",
-          songs: albumData.tracks.items.map((track) => ({
-            name: track.name,
-            duration_ms: track.duration_ms,
-            track_number: track.track_number,
-            preview_url: track.preview_url,
-          })),
-        };
-      })
-      .catch(async (error) => {
-        if (error.code === "ETIMEDOUT") {
-          console.warn(`Timeout fetching album details: ${albumId}`);
-          return this.delay(1000).then(() => this.getAlbumDetails(albumId));
-        }
-        if (error?.response?.status === 429) {
-          const retryAfter =
-            (error.response.headers?.["retry-after"] || 10) + 1;
-          console.warn(`Rate limit exceeded, retrying in ${retryAfter}s...`);
+    if (this.existingData?.spotify_top_genre_artists) {
+      const genre = this.existingData.spotify_top_genre_artists.find(
+        (item) => item.genre_name === genreId,
+      );
 
-          return this.delay(retryAfter * 1000).then(() =>
-            this.getAlbumDetails(albumId),
-          );
-        } else {
-          throw error;
-        }
-      });
+      const artist = genre?.artists?.find((artist) => artist.id === artistId);
+
+      const album = artist?.albums?.find((album) => album.id === albumId);
+      if (album) {
+        console.log(`Using existing data for album: ${album.name}`);
+        return album;
+      }
+    }
+
+    return this.makeRequest(url, async (response) => {
+      const albumData = response.data;
+      return {
+        id: albumData.id,
+        name: albumData.name,
+        release_date: albumData.release_date,
+        total_tracks: albumData.total_tracks,
+        popularity: albumData.popularity || 0,
+        album_type: albumData.album_type,
+        cover_image: albumData.images[0]?.url || "",
+        songs: albumData.tracks.items.map((track) => ({
+          name: track.name,
+          duration_ms: track.duration_ms,
+          track_number: track.track_number,
+          preview_url: track.preview_url,
+        })),
+      };
+    });
   }
 
-  async getArtistDetails(artist) {
+  async getArtistDetails(artist, genreId) {
     const artistData = {
+      id: artist.id,
       name: artist.name,
       popularity: artist.popularity,
       followers: artist.followers.total,
@@ -207,8 +212,7 @@ class SpotifyDataCollector {
       albums: [],
     };
 
-    const albums = await this.getArtistAlbums(artist.id);
-    artistData.albums = albums;
+    artistData.albums = await this.getArtistAlbums(artist.id, genreId);
     return artistData;
   }
 
@@ -217,7 +221,7 @@ class SpotifyDataCollector {
     const genreData = { genre_name: genre, artists: [] };
 
     for (const artist of artists.slice(0, ARTIST_PER_GENRE_LIMIT)) {
-      const artistData = await this.getArtistDetails(artist);
+      const artistData = await this.getArtistDetails(artist, genre);
       genreData.artists.push(artistData);
     }
 
@@ -247,6 +251,7 @@ export async function GetMusicData() {
     await retrieveApiKeys();
   const collector = new SpotifyDataCollector(clientId, clientSecret);
   await collector.getToken();
+  await collector.getExistingData();
   const data = await collector.collectAllData();
   return {
     data,
